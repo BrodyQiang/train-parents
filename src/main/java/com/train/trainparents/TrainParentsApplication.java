cfaec5e09b1bdb2677b54dfe70d9a48c71da8748
fly-pig-deep <1322423039@qq.com>
2023-04-18 10:22:45 +0800

    insert

---
+++ src/main/java/com/train/trainparents/TrainParentsApplication.java
@@ -0,0 +1,13 @@
+package com.train.trainparents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainParentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainParentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/train/trainparents/TrainParentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.trainparents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainParentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
