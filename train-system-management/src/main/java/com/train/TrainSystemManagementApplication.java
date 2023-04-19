542d77c3c69ac9a1e496ac99c407273a9eeaf59d
fly-pig-deep <1322423039@qq.com>
2023-04-19 15:11:30 +0800

    insert 系统支撑 (权限,登录,乘客等信息)

---
+++ train-system-management/src/main/java/com/train/TrainSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSystemManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainSystemManagementApplication.class, args);
+    }
+
+}
