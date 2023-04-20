33cb1e9bc69e75ed9b6179805c976940c1f90005
Fiy-pig-deep <1322423039@qq.com>
2023-04-20 21:00:06 +0800

    项目各个模块的初始化

--- train-gateway/src/main/java/com/train/gateway/GatewayApplication.java
+++ train-gateway/src/main/java/com/train/gateway/GatewayApplication.java
@@ -1,22 +1,21 @@
-package com.train;
+package com.train.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class TrainSystemManagementApplication {
+public class GatewayApplication {
 
-    //日志管理
-    private static final Logger LOG = LoggerFactory.getLogger(TrainSystemManagementApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(TrainSystemManagementApplication.class);
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("系统支撑服务地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("网关地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
---
+++ train-gateway/src/main/java/com/train/gateway/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.train.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr.Liu
+ * @email yxml2580@163.com
+ * @createDate 2023/4/20 19:55
+ */
+@RestController
+@RequestMapping("/account")
+public class TestController {
+
+    @GetMapping("/name")
+    public String test() {
+        return "Holle World";
+    }
+
+}
