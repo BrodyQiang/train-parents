cd87b60f700e05ccd92b5d5f9d3fd33ab3e7d6d6
BrodyQiang <1322423039@qq.com>
2023-05-03 21:02:02 +0800

    新增business业务模块

---
+++ train-business/src/main/java/com/train/TrainBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBusinessApplication.class, args);
+    }
+
+}
