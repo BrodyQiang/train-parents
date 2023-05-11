659c789863b89976492452593972c85d5661c72e
BrodyQiang <1322423039@qq.com>
2023-05-11 19:11:18 +0800

    修改本地仓库 并调试代码是否能运行

--- train-batch/src/main/java/com/train/TrainBatchApplication.java
+++ train-batch/src/main/java/com/train/TrainBatchApplication.java
@@ -1,13 +1,24 @@
 package com.train;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TrainBatchApplication {
 
+    //日志管理
+    private static final Logger LOG = LoggerFactory.getLogger(TrainBatchApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(TrainBatchApplication.class, args);
+        SpringApplication app = new SpringApplication(TrainBatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Job跑批服务地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        System.out.println("-----项目启动成功-----");
+        System.out.printf("Job跑批服务地址: \thttp://127.0.0.1:%s", env.getProperty("server.port"));
     }
 
 }
