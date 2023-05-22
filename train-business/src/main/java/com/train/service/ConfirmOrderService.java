d80305bad0310c1be459a40ee4422cbcbbf76f8e
BrodyQiang <1322423039@qq.com>
2023-05-22 19:28:44 +0800

    预扣减余票数量，并判断余票是否足够

--- train-business/src/main/java/com/train/service/ConfirmOrderService.java
+++ train-business/src/main/java/com/train/service/ConfirmOrderService.java
@@ -2,13 +2,17 @@ package com.train.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.train.bean.request.ConfirmOrderQueryReq;
 import com.train.bean.request.ConfirmOrderSaveReq;
+import com.train.bean.request.ConfirmOrderTicketReq;
 import com.train.bean.response.ConfirmOrderQueryRes;
 import com.train.common.context.LoginAccountContext;
+import com.train.common.enums.BusinessExceptionEnum;
+import com.train.common.exception.BusinessException;
 import com.train.common.interceptor.AccountInterceptor;
 import com.train.common.response.DBPages;
 import com.train.common.util.SnowUtil;
@@ -16,6 +20,7 @@ import com.train.domain.ConfirmOrder;
 import com.train.domain.ConfirmOrderExample;
 import com.train.domain.DailyTrainTicket;
 import com.train.enums.ConfirmOrderStatusEnum;
+import com.train.enums.SeatTypeEnum;
 import com.train.mapper.ConfirmOrderMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -117,6 +122,63 @@ public class ConfirmOrderService {
         DailyTrainTicket dailyTrainTicket = dailyTrainTicketService.selectByUnique(date, trainCode, start, end);
         LOG.info("查出余票记录：{}", dailyTrainTicket);
 
+        // 预扣减余票数量，并判断余票是否足够
+        reduceTickets(bean, dailyTrainTicket);
 
+    }
+
+    /**
+     * 预扣减余票数量，并判断余票是否足够
+     *
+     * @param bean
+     * @param dailyTrainTicket
+     */
+    private void reduceTickets(ConfirmOrderSaveReq bean, DailyTrainTicket dailyTrainTicket) {
+        for (ConfirmOrderTicketReq ticketReq : bean.getTickets()) {
+            String seatTypeCode = ticketReq.getSeatTypeCode();
+            SeatTypeEnum seatTypeEnum = EnumUtil.getBy(SeatTypeEnum::getCode, seatTypeCode);
+            switch (seatTypeEnum) {
+                case YDZ : {
+                    // 一等座
+                    int countLeft = dailyTrainTicket.getYdz() - 1; // 预扣减
+                    if (countLeft < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    // 更新余票
+                    dailyTrainTicket.setYdz(countLeft);
+                    break;
+                }
+                case EDZ : {
+                    // 二等座
+                    int countLeft = dailyTrainTicket.getEdz() - 1;// 预扣减
+                    if (countLeft < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    // 更新余票
+                    dailyTrainTicket.setEdz(countLeft);
+                    break;
+                }
+                case RW : {
+                    // 软卧
+                    int countLeft = dailyTrainTicket.getRw() - 1;// 预扣减
+                    if (countLeft < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    // 更新余票
+                    dailyTrainTicket.setRw(countLeft);
+                    break;
+                }
+                case YW : {
+                    // 硬卧
+                    int countLeft = dailyTrainTicket.getYw() - 1;// 预扣减
+                    if (countLeft < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    // 更新余票
+                    dailyTrainTicket.setYw(countLeft);
+                    break;
+                }
+            }
+        }
     }
 }
